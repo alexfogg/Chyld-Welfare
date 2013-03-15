b8c7b3b7687227c5a2bdac405363b2ebb2305215
Matt Galvin <matthewgalvin908@gmail.com>
2013-03-15 17:46:34 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chyldwelfare::Application.config.secret_token = '20432ff3ac755cea185a9e9dc289d927331a2a64101381af1475beeb53a5944d77b95d8d63e586fb66251a25a8ff92a25984a9deef2b8ca0e5f2ec1b52f93687'
