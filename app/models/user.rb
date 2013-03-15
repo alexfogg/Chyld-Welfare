f2e8f0563e3a8a8d0b2b4417e782efaea731d96f
Blanchet <h55nick@gmail.com>
2013-03-15 18:20:21 -0400

    defualting app/doner

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :name, :email, :password, :password_confirmation, :is_doner, :is_app, :bio, :photo
+  before_save :default_values
+
+
+  private
+  def default_values
+    self.is_doner ||= false
+    self.is_app ||=false
+  end
+
 end
