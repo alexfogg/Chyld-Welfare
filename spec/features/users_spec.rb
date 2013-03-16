d63daffb21181648fb75301f4cff395ea13c911b
Matt Galvin <matthewgalvin908@gmail.com>
2013-03-16 15:08:07 -0400

    made some changes to tests, button names

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -12,16 +12,16 @@ describe 'Users' do
       it 'after filling the form and clicking create account, user should be createsd',:js => true do
           visit root_path
           click_link('Create Account')
-          fill_in('Username',:with=>'Bob')
-          fill_in('Email',:with=>'Bob@gmail.com')
+          fill_in('user_name',:with=>'Bob')
+          fill_in('user_email',:with=>'Bob@gmail.com')
           fill_in('user_password',:with=>'a')
           fill_in('user_password_confirmation',:with=>'a')
           click_button('Create Account')
             #Tests
-            page.should have_text('You have created an account!') #testing to make sure you have confirmation showing
+            page.should have_text('You have successfully created an account!') #testing to make sure you have confirmation showing
             page.should have_button('Login') #Testing to make sure you have login form showing
             page.should_not have_button('Create Account') #testing to make sure you don't have the create form still up
-            expect(User.first.user.username).to eq 'Bob' #testing to make sure it's in database (this should happen before the above but it might cause a race condition)
+            expect(User.first.user.name).to eq 'Bob' #testing to make sure it's in database (this should happen before the above but it might cause a race condition)
       end
 
     end
